8624ce039ab3ffcad6c170d271d39681ac64e6c3
kkodu <nawy1130@gmail.com>
2018-05-30 17:41:03 +0900

    add files

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express')
-var app = express()
+var bodyParser = require('body-parser')
 
+var app = express()
 var port = process.env.PORT || 5000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/keyboard', function (req, res) {
   var data = {
@@ -9,6 +13,40 @@ app.get('/keyboard', function (req, res) {
     buttons: ['최신 영화', '최애 영화', '흥얼흥얼']
   }
   res.json(data)
+})
+
+app.post('/message', function (req, res) {
+  var msg = req.body.content
+  console.log('전달 받은 키워드 : ' + msg)
+
+  var responseMsg
+  switch (msg) {
+  case '최신 영화':
+    responseMsg = {
+      message: {
+        text: '독전을 추천드려요!'
+      }
+    }
+    break
+  
+  case '최애 영화':
+    responseMsg = {
+      message: {
+        text: '아이언맨3 추천드려요!'
+      }
+    }
+    break
+  
+  default:
+    responseMsg = {
+      message: {
+        text: '흥얼흥얼 :)'
+      }
+    }
+    break
+  }
+
+  res.json(responseMsg)
 })
 
 app.listen(port, function () {
